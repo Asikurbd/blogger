5b13748c6584b252df49b7becf511c77f544c0af
Asikur Rahman <asikurbd25@gmail.com>
2024-10-28 14:06:40 +0600

    Create blogger-url-shortner.js

---
+++ blogger-url-shortner.js
@@ -0,0 +1,20 @@
+
+//<![CDATA[
+var key = window.location.href.split("go/")[1].replace("/","")
+var urls={
+
+  
+'fb':'https://www.facebook.com/arbd0',
+'ig':'https://www.instagram.com/sysa_shoaiyb',
+'twitter':"https://twitter.com/shoaiybsysa",
+'gh':"https://github.com/shoaiyb"
+  
+}
+if(key){
+if(urls[key]){
+window.location.href=urls[key]
+}else{
+document.write("'"+key+"' not found :(");
+}
+}
+//]]>
