57341dafb68f5712078a3c1171dce8ae633c7ed2
Asikur Rahman <asikurbd25@gmail.com>
2024-10-28 15:45:14 +0600

    Update blogger-url-shortner.js

--- blogger-url-shortner.js
+++ blogger-url-shortner.js
@@ -1,19 +1,31 @@
+<script language='javascript'>
+
 //<![CDATA[
-var key = window.location.href.split("go/")[1].replace("/","")
+var key = window.location.href.split("go/")[1].replace("?m=1","")
 var urls={
 
-  
+
+
 'fb':'https://www.facebook.com/arbd0',
+
+
 'ig':'https://www.instagram.com/sysa_shoaiyb',
+
+
 'soft':"https://arbd99.blogspot.com/p/pc-softwore.html",
+
+
 'gh':"https://github.com/asikurbd"
-  
+
+
+
 }
 if(key){
 if(urls[key]){
 window.location.href=urls[key]
 }else{
-document.write("'"+key+"' not fkkound :(");
+document.write("'"+key+"' not found :(");
 }
 }
 //]]>
+</script>
