98421b8af2507daf3c55d3e86552ae9e8e158cb8
Asikur Rahman <asikurbd25@gmail.com>
2024-11-10 13:04:17 +0600

    Create copy.js

---
+++ copy.js
@@ -0,0 +1,30 @@
+
+//<![CDATA[
+var key = window.location.href.split("g/")[1].replace("?m=1","")
+var urls={
+
+
+'idm':"https://arbd99.blogspot.com/2024/11/idm-64223-pre-activated.html",
+
+'yt':"https://youtube.com",
+
+'fb':"https://www.facebook.com/arbd0",
+
+'gl':"https://www.google.com",
+
+'xm':"https://xhamster.desi",  
+
+'soft':"https://arbd99.blogspot.com/p/pc-softwore.html",
+
+'gh':"https://github.com/asikurbd"
+
+
+}
+if(key){
+if(urls[key]){
+window.location.href=urls[key]
+}else{
+document.write("'"+key+"' not found :(");
+}
+}
+//]]>
