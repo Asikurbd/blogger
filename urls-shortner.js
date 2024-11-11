955c1eadd2f490ce682194120abaf9e65c21b64c
Asikur Rahman <asikurbd25@gmail.com>
2024-11-11 17:50:01 +0600

    Update urls-shortner.js

--- urls-shortner.js
+++ urls-shortner.js
@@ -4,6 +4,9 @@ var key = window.location.href.split("g/")[1].replace("?m=1","")
 var urls={
 
 
+
+'app':"https://asikurbd.github.io/Application-Copy",
+
 'idm':"https://arbd99.blogspot.com/2024/11/idm-64223-pre-activated.html",
 
 'yt':"https://youtube.com",
