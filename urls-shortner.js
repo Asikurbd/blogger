d3fedb6ca1f25d4797e37408a6e24bc932095c84
Asikur Rahman <asikurbd25@gmail.com>
2024-11-19 10:09:53 +0600

    Update urls-shortner.js

--- urls-shortner.js
+++ urls-shortner.js
@@ -5,7 +5,7 @@ var urls={
 
 
 
-'note':"https://arbd99.blogspot.com/p/my-note.html", 
+'note':"https://arbd99.blogspot.com/p/my-note-xjsjgkmgfkgfufdkndfgn.html", 
 
 'app':"https://asikurbd.github.io/Application-Copy",
 
